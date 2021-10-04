57f35da3296686bafc250e4a5be1a6a877b3f53a
Amanur Rahman <aaman.sunny007@gmail.com>
2021-10-04 20:08:10 +0600

    Day 4: Project Frontend Bootcamp

---
+++ frontend_bootcamp/assets/js/script.js
@@ -0,0 +1,8 @@
+mapboxgl.accessToken = 'pk.eyJ1IjoiYWFtYW4wMDciLCJhIjoiY2t1Y3BjN3Y5MTJ1eDMxbzM0N3o5a2oybSJ9.5MO9A4xEcRgn858UnJftlQ';
+
+var map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/streets-v11',
+    center: [-71.060982, 42.35725],
+    zoom: 15
+});
